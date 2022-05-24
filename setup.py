f2f621bd9cc50761cd3f83b19297eaae0754fb35
erguntiryaki <erguntiryaki27@gmail.com>
2022-05-24 23:54:04 +0300

    Initialize the repository.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='flowcytopy',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Ergün Tiryaki',
+    author_email='erguntiryaki27@gmail.com',
+    description='A package for alternative visualizations and analyses of cytometry data in python environment.'
+)
