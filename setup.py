5ea7af7f78b4c23801ecd59a81aadc230aad6e20
erguntiryaki <erguntiryaki27@gmail.com>
2022-05-25 22:28:25 +0300

    update setup script

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='flowcytopy',
     version='0.0.1',
-    packages=[''],
+    packages=find_packages(include=['flowcytopy', 'flowcytopy.*']),
     url='',
-    install_requires=['pacmap', 'trimap', 'anndata']
+    install_requires=['pacmap', 'trimap', 'anndata'],
     license='',
     author='Ergün Tiryaki',
     author_email='erguntiryaki27@gmail.com',
